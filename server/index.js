9fab844efd6feb0d852c59e2a60117e92e232f10
betonit007 <timnagorski@hotmail.com>
2021-05-03 10:23:09 -0400

    frontend subscriptions connected!

--- server/index.js
+++ server/index.js
@@ -20,9 +20,6 @@ const pubsub = new PubSub()
 const typeDefs = mergeTypeDefs(loadFilesSync(path.join(__dirname, './typeDefs')))
 const resolvers = mergeResolvers(loadFilesSync(path.join(__dirname, './resolvers')))
 
-
-
-
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
